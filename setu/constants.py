4e64d91997aef22f0ba188d2d4da9b7c58244894
Samir Patil <samirspatil742099@gmail.com>
2023-06-07 18:45:12 +0530

    [CHANGES] Minor changes

--- setu/constants.py
+++ setu/constants.py
@@ -1,15 +1,16 @@
 from enum import Enum
 from typing import final
+from decouple import config
+
 
 SETU_API_ENDPOINT: final(str) = "https://fiu-uat.setu.co/"
 
-X_CLIENT_ID = "255d0b6c-492d-44cf-8581-e9494c7b0914"
-X_CLIENT_SECRET = "9c2fc756-3d17-46d6-b28d-be4d71953e83"
-
+X_CLIENT_ID = config('X_CLIENT_ID')
+X_CLIENT_SECRET = config('X_CLIENT_SECRET')
 
 SETU_API_HEADERS = {
-  'x-client-id': 'dc692d3c-b7c9-4123-bcd9-0ee5360b5fdf',
-  'x-client-secret': '5982f80d-1ea0-4856-a669-41efa576388c',
+  'x-client-id': X_CLIENT_ID,
+  'x-client-secret': X_CLIENT_SECRET,
   'Content-Type': 'application/json'
 }
 
